6fae50b24a29520eac51e1eed4b8bd2c9e8e861e
DavyAndrade <davyandrade.dev@gmail.com>
2026-02-26 19:08:16 -0300

    first commit

---
+++ src/components/react/Welcome.tsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+  return (
+    <div className="min-h-full bg-zinc-950 flex justify-center items-center flex-col gap-4">
+      <h1 className="text-white text-5xl">Davy Andrade</h1>
+      <p className="text-white">Desenvolvedor Front-end</p>
+    </div>
+  );
+}
